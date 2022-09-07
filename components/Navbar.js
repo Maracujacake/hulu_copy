ea175e35f92e2c16049e044f2536abb114cc596e
Maracujacake <madlordx@gmail.com>
2022-09-07 17:43:47 -0300

    versao 1

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import requests from "../utilities/requests";
+import { useRouter } from "next/dist/client/router";
+
+function Navbar() {
+
+  const router = useRouter();
+
+  return (
+    <nav className='relative'>
+        <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
+            {Object.entries(requests).map(( [key, {title, url }] ) => (
+                 (<h2 
+                            key={key} 
+                            onClick={() => {
+                                router.push(`/?genre=${key}`);
+                            }}
+                            className='
+                               cursor-pointer
+                               transition
+                               duration-100
+                               transform hover:scale-120 hover:text-white
+                               active:text-green-500'
+                               > {title} </h2>
+                        )
+            ))}
+        </div>
+        <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202a] h-10 w-1/12 " />
+        <div className="absolute top-0 bg-gradient-to-r from-black h-10 w-1/12 " />
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ components/Thumbnail.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import { ThumbUpIcon } from '@heroicons/react/solid'
+import { forwardRef } from 'react'
+
+const Thumbnail = forwardRef(( {result}, ref ) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  return (
+    <div ref={ref} className="cursor-pointer group p-2 transition duration-500 ease-in transform sm:hover:scale-105 hover:z-30">
+        <Image layout='responsive'
+               height={1080}
+               width={1920} 
+               src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.backdrop_path}`}
+        />
+        <div className='p-2'>
+            <p className='truncate max-w-md'>{result.overview}</p>
+            <h2 className="mt-1
+                           text-2xl
+                           text-white 
+                           transition-all 
+                           duration-100 
+                           ease-in-out 
+                           group-hover:font-bold
+                           group-active:text-green-500">{result.title} || {result.original_name}
+            </h2>
+            <p className="flex 
+                          items-center 
+                          opacity-0 
+                          group-hover:opacity-100
+                          group-hover:text-green-500 
+                          transition duration-700"> {result.media_type && `${result.media_type} `}{" "}
+                {result.release_date || result.first_air_date} •
+                <ThumbUpIcon className="h-5 mx-1" /> {result.vote_count}
+            </p>
+        </div>
+    </div>
+  )
+})
+
+export default Thumbnail
